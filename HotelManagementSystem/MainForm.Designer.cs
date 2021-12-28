f6dbd616579017363ff6454b9942ba06586dae8c
MahmoudHamdy00 <Mahmoudh.Morsy@gmail.com>
2021-12-29 00:53:27 +0200

    add usercontrols to mainForm

--- HotelManagementSystem/MainForm.Designer.cs
+++ HotelManagementSystem/MainForm.Designer.cs
@@ -28,21 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.paymentBtn = new System.Windows.Forms.Button();
+            this.roomBtn = new System.Windows.Forms.Button();
+            this.ReservationBtn = new System.Windows.Forms.Button();
+            this.guestBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.settingsBtn);
+            this.panel1.Controls.Add(this.paymentBtn);
+            this.panel1.Controls.Add(this.roomBtn);
+            this.panel1.Controls.Add(this.ReservationBtn);
+            this.panel1.Controls.Add(this.guestBtn);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 579);
+            this.panel1.TabIndex = 0;
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.settingsBtn.Location = new System.Drawing.Point(23, 447);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(223, 48);
+            this.settingsBtn.TabIndex = 4;
+            this.settingsBtn.Text = "Settings";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            this.settingsBtn.Click += new System.EventHandler(this.settingsBtn_Click);
+            // 
+            // paymentBtn
+            // 
+            this.paymentBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.paymentBtn.Location = new System.Drawing.Point(23, 393);
+            this.paymentBtn.Name = "paymentBtn";
+            this.paymentBtn.Size = new System.Drawing.Size(223, 48);
+            this.paymentBtn.TabIndex = 3;
+            this.paymentBtn.Text = "Payment";
+            this.paymentBtn.UseVisualStyleBackColor = true;
+            this.paymentBtn.Click += new System.EventHandler(this.paymentBtn_Click);
+            // 
+            // roomBtn
+            // 
+            this.roomBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.roomBtn.Location = new System.Drawing.Point(23, 339);
+            this.roomBtn.Name = "roomBtn";
+            this.roomBtn.Size = new System.Drawing.Size(223, 48);
+            this.roomBtn.TabIndex = 2;
+            this.roomBtn.Text = "Room";
+            this.roomBtn.UseVisualStyleBackColor = true;
+            this.roomBtn.Click += new System.EventHandler(this.roomBtn_Click);
+            // 
+            // ReservationBtn
+            // 
+            this.ReservationBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ReservationBtn.Location = new System.Drawing.Point(23, 285);
+            this.ReservationBtn.Name = "ReservationBtn";
+            this.ReservationBtn.Size = new System.Drawing.Size(223, 48);
+            this.ReservationBtn.TabIndex = 1;
+            this.ReservationBtn.Text = "Reservation";
+            this.ReservationBtn.UseVisualStyleBackColor = true;
+            this.ReservationBtn.Click += new System.EventHandler(this.ReservationBtn_Click);
+            // 
+            // guestBtn
+            // 
+            this.guestBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.guestBtn.Location = new System.Drawing.Point(23, 231);
+            this.guestBtn.Name = "guestBtn";
+            this.guestBtn.Size = new System.Drawing.Size(223, 48);
+            this.guestBtn.TabIndex = 0;
+            this.guestBtn.Text = "Guest";
+            this.guestBtn.UseVisualStyleBackColor = true;
+            this.guestBtn.Click += new System.EventHandler(this.guestBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Location = new System.Drawing.Point(255, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1057, 579);
+            this.panel2.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1312, 579);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button settingsBtn;
+        private Button paymentBtn;
+        private Button roomBtn;
+        private Button ReservationBtn;
+        private Button guestBtn;
+        private Panel panel2;
     }
 }
