7e80f71dcfd8f1048f2f24554b6ca0dab105f2c9
MahmoudHamdy00 <Mahmoudh.Morsy@gmail.com>
2021-12-29 00:46:54 +0200

    add pyment text

--- HotelManagementSystem/UserControls/PaymentUserControl.Designer.cs
+++ HotelManagementSystem/UserControls/PaymentUserControl.Designer.cs
@@ -28,10 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1614, 189);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "paymentUserControl";
+            // 
+            // PaymentUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "PaymentUserControl";
+            this.Size = new System.Drawing.Size(1024, 587);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
     }
 }
