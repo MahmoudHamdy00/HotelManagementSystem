082732b06464d1386f57c8d60bfe299224c904cb
MahmoudHamdy00 <Mahmoudh.Morsy@gmail.com>
2021-12-29 02:51:12 +0200

    change starting form

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -11,7 +11,8 @@ namespace HotelManagementSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
+            // Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
