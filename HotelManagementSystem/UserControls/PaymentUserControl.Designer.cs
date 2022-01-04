93201c7c70edb1a357d35ddf71988519c22fb079
Abdo <MohammedM.Abdeltawab@gmail.com>
2022-01-04 06:09:19 +0200

    design payment

--- HotelManagementSystem/UserControls/PaymentUserControl.Designer.cs
+++ HotelManagementSystem/UserControls/PaymentUserControl.Designer.cs
@@ -43,79 +43,88 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dataGridView1.Location = new System.Drawing.Point(490, 4);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(394, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(608, 640);
+            this.dataGridView1.Size = new System.Drawing.Size(567, 483);
             this.dataGridView1.TabIndex = 8;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // SSNTextBox
             // 
-            this.SSNTextBox.Location = new System.Drawing.Point(160, 85);
-            this.SSNTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SSNTextBox.Location = new System.Drawing.Point(157, 73);
             this.SSNTextBox.Name = "SSNTextBox";
             this.SSNTextBox.PlaceholderText = "Type an SSN then press Enter";
-            this.SSNTextBox.Size = new System.Drawing.Size(238, 27);
+            this.SSNTextBox.Size = new System.Drawing.Size(209, 23);
             this.SSNTextBox.TabIndex = 9;
             this.SSNTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tab2SSNTextBox_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 89);
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(26, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.Size = new System.Drawing.Size(47, 21);
             this.label1.TabIndex = 10;
             this.label1.Text = "SSN:";
             // 
             // reservationLabel
             // 
             this.reservationLabel.AutoSize = true;
-            this.reservationLabel.Location = new System.Drawing.Point(235, 196);
+            this.reservationLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.reservationLabel.Location = new System.Drawing.Point(157, 161);
             this.reservationLabel.Name = "reservationLabel";
-            this.reservationLabel.Size = new System.Drawing.Size(16, 20);
+            this.reservationLabel.Size = new System.Drawing.Size(12, 15);
             this.reservationLabel.TabIndex = 30;
             this.reservationLabel.Text = "?";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(94, 196);
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(26, 155);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 20);
+            this.label5.Size = new System.Drawing.Size(121, 21);
             this.label5.TabIndex = 29;
             this.label5.Text = "Reservation Id:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(94, 285);
+            this.label8.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(26, 222);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(81, 20);
+            this.label8.Size = new System.Drawing.Size(92, 21);
             this.label8.TabIndex = 35;
             this.label8.Text = "Total Price:";
             // 
             // totalPriceLabel
             // 
             this.totalPriceLabel.AutoSize = true;
-            this.totalPriceLabel.Location = new System.Drawing.Point(247, 285);
+            this.totalPriceLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.totalPriceLabel.Location = new System.Drawing.Point(157, 228);
             this.totalPriceLabel.Name = "totalPriceLabel";
-            this.totalPriceLabel.Size = new System.Drawing.Size(16, 20);
+            this.totalPriceLabel.Size = new System.Drawing.Size(12, 15);
             this.totalPriceLabel.TabIndex = 36;
             this.totalPriceLabel.Text = "?";
             // 
             // PaymentUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
             this.Controls.Add(this.totalPriceLabel);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.reservationLabel);
@@ -123,8 +132,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.SSNTextBox);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PaymentUserControl";
-            this.Size = new System.Drawing.Size(1101, 648);
+            this.Size = new System.Drawing.Size(963, 486);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
